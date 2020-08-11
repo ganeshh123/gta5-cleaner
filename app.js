462c3a4ede5056f8f14cf14e777c04564b5b97a2
Ganesh H <43777439+ganeshh123@users.noreply.github.com>
2020-08-11 20:07:11 +0100

    Working clean/restore

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 // Require Filesystem
 let fileSystem = require('fs')
 let path = require('path')
+let readline = require('readline')
+
+/* Interface for reading input */
+let terminal = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
 
 /* Import Files to Keep */
 let essentialFiles = require('./essentialFiles')
 
- let cleanMods = (filePath) => {
+let cleanMods = (filePath) => {
     if (fileSystem.existsSync(filePath)) {
       /* Ensure that software files and generated PDFs are not deleted */
         let files = fileSystem.readdirSync(filePath).filter((file) => {
@@ -21,20 +28,65 @@ let essentialFiles = require('./essentialFiles')
             files[index] = path.join(filePath, file)
         })
 
-        console.log('Files to clean')
-        console.log(files)
-
-        console.log('New Paths')
-        console.log(newPaths)
-
         fileSystem.mkdirSync(path.join(filePath, '/modstore/'))
 
         files.forEach((file, index) => {
             fileSystem.renameSync(file, newPaths[index])
         })
 
-        console.log('Moved')
+        console.log('Moved mods to modstore folder.')
+        console.log('You can now play Online.')
     }
 }
 
-cleanMods(process.cwd())
+let restoreMods = (filePath) => {
+    filePathMods = path.join(filePath, '/modstore/')
+    
+    if (fileSystem.existsSync(filePathMods)){
+        
+        let files = fileSystem.readdirSync(filePathMods)
+        
+        let newPaths = []
+
+        files.forEach((file, index) => {
+            let newPath = path.join(filePath, file)
+            newPaths.push(newPath)
+            files[index] = path.join(filePathMods, file)
+        })
+
+        files.forEach((file, index) => {
+            fileSystem.renameSync(file, newPaths[index])
+        })
+
+        console.log('Restored Mods to GTA V!')
+        fileSystem.rmdirSync(filePathMods)
+
+
+    }else{
+        console.log('Unable to restore mods, modstore folder does not exist!')
+    }
+}
+
+console.log('\nGTA V Mod Cleaner')
+console.log('\nWould you like to "clean" mods from GTA V for GTA Online, or "restore" mods for Single Player?')
+console.log('\nType "clean" to clean mods and press Enter')
+console.log('Type "restore" to restore mods and press Enter')
+terminal.question('\nYour Choice : ', (choice) => {
+
+    terminal.close();
+
+    if(choice === 'clean'){
+        cleanMods(process.cwd())
+    }else if(choice === 'restore'){
+        restoreMods(process.cwd())
+    }else{
+        console.log('Invalid Choice')
+    }
+
+    console.log('\nPress any key to exit');
+
+    process.stdin.setRawMode(true);
+    process.stdin.resume();
+    process.stdin.on('data', process.exit.bind(process, 0));
+
+});
